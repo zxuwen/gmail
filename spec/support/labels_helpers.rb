cbe067c0f4f281cbc2936ca0740521c47fa17e98
Wyn Zhang <wyn@roadhop.com>
2013-11-01 17:30:08 +0800

    Edited tests.

--- spec/support/labels_helpers.rb
+++ spec/support/labels_helpers.rb
@@ -1,16 +1,13 @@
 shared_examples_for "a label manipulator" do
-  
-  before { subject.create("TEST") }
-  after { subject.delete("TEST") }
 
   it "should get list of all available labels" do
     labels = subject
-    labels.all.should include("TEST", "INBOX")
+    labels.all.should include("[Gmail]/Drafts", "INBOX")
   end
 
   it "should be able to check if there is given label defined" do
     labels = subject
-    labels.exists?("TEST").should be_true
+    labels.exists?("INBOX").should be_true
     labels.exists?("FOOBAR").should be_false
   end
 
